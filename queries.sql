e548b603cb068933e40cc6dbb223207c2dfd8344
Juan M Guevara Martinez <juanmanuelguevara@live.com>
2020-02-29 03:06:46 -0800

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -53,9 +53,39 @@ CREATE TABLE titles (
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
+-- Table Print section
+
 SELECT * FROM departments;
 SELECT * FROM dept_emp;
 SELECT * FROM dept_manager;
 SELECT * FROM employees;
 SELECT * FROM salaries;
 SELECT * FROM titles;
+
+-- Retirement Eligibility
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1953-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1954-12-31';
+
+-- More Narrow Search for Retirement Eligibility
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
